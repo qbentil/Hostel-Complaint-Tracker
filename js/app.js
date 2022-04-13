0d96fc8a78d3a95ec39dfb7edaa15e0e68a43312
qbentil <Bentilshadrack72@gmail.com>
2022-04-13 01:52:47 +0000

    Create Validate form Function

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Form Validation:
+const sName = document.getElementById('sName');
+const sID = document.getElementById('sID');
+const rNum = document.getElementById('rNum');
+const rMsg = document.getElementById('rMsg');
+
+function validate_form()
+{
+    let hasError = false;
+}

3333a36d8ebb705ef6f02a3cb81895eb9bdd546a
qbentil <Bentilshadrack72@gmail.com>
2022-04-14 15:56:37 +0000

    App JS

--- js/app.js
+++ js/app.js
@@ -7,7 +7,16 @@ const alert = document.getElementsByClassName('alert')[0]; //Get alert box
 
 
 alert.classList.add("alert-danger");
+alert.style.display = 'none'
+
+
+document.getElementById("btn_send").addEventListener('click', validate_form)
 function validate_form()
 {
+    
     let hasError = false;
+
+    // Check for name
+    hasError = sName.value == ""? true: false
+    console.log(hasError);
 }

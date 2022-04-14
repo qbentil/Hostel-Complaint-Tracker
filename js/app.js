e4555dd034d8c0110ad929daf62112b31a25fb3b
qbentil <Bentilshadrack72@gmail.com>
2022-04-14 17:12:46 +0000

    Handle numeric inputs

--- js/app.js
+++ js/app.js
@@ -4,20 +4,32 @@ const sID = document.getElementById('sID');
 const rNum = document.getElementById('rNum');
 const rMsg = document.getElementById('rMsg');
  //Get alert box
-const alert = document.getElementsByClassName('alert')[0];
+const alert_ = document.getElementsByClassName('alert')[0];
 const alert_type = document.getElementById('alert-type');
 const alert_msg = document.getElementById('alert-msg');
 
 
-alert.classList.add("alert-danger");
-alert.style.display = 'none'
+alert_.classList.add("alert-danger");
+alert_.style.display = 'none'
 
 document.getElementById("btn_send").addEventListener('click', validate_form)
 function validate_form()
 {
     
-    let hasError =  handle_input_error(sName, "Enter your full name");
-    handle_input_error(sID, "Enter your full name");
+    let hasError = input_handler(sName, "Enter your full name");
+    if(!hasError)
+    {
+        hasError = input_handler(sID, "Enter your student ID", true);
+    }
+    if(!hasError)
+    {
+        hasError = input_handler(rNum, "Enter your room number", true)
+    }
+
+    if(!hasError)
+    {
+        hasError = input_handler(rMsg, "Enter your report message")
+    }
 
 
     
@@ -30,22 +42,44 @@ function flag_alert(msg, type = 'ERROR')
 {
     alert_type.innerText = type
     alert_msg.innerText = msg;
-    type == "ERROR"? alert.classList.add("alert-danger"): alert.classList.add("alert-success")
-    type == "ERROR"? alert.style.display = 'block': alert.style.display = 'none'
+    type == "ERROR"? alert_.classList.add("alert-danger"): alert_.classList.add("alert-success")
+    type == "ERROR"? alert_.style.display = 'block': alert_.style.display = 'none'
 }
 
-function handle_input_error(input, msg)
+function input_handler(input, msg, isNum = false)
 {
     let type = "SUCCESS"
-    if(input.value == "")
+    if(isNum == true)
     {
-        input.style.borderColor = "#f00"
-        type = "ERROR"
-        flag_alert(msg, type)
-        return true
+        if(input.value == "")
+        {
+            input.style.borderColor = "#f00"
+            type = "ERROR"
+            flag_alert(msg, type)
+            return true
+        }else if(isNaN(parseFloat(input.value)))
+        {
+            input.style.borderColor = "#f00"
+            type = "ERROR"
+            flag_alert("Field must be a number", type)
+            return true 
+        }else{
+            input.style.borderColor = "#0f0"
+            flag_alert(msg, type)
+            return false
+        }
     }else{
-        input.style.borderColor = "#0f0"
-        flag_alert(msg, type)
-        return false
+        
+        if(input.value == "")
+        {
+            input.style.borderColor = "#f00"
+            type = "ERROR"
+            flag_alert(msg, type)
+            return true
+        }else{
+            input.style.borderColor = "#0f0"
+            flag_alert(msg, type)
+            return false
+        }
     }
 }
